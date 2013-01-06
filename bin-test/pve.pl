0d3f019769536ba334647b4eda926d251e154580
papamoose <philk1989@gmail.com>
2013-01-06 16:00:13 -0600

    Adding my perl adaptation of proxbash. WIP

--- bin-test/pve.pl
+++ bin-test/pve.pl
@@ -1,8 +1,18 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
 
 use Net::Proxmox::VE;
+use Net::Proxmox::VE::Access;
+use Net::Proxmox::VE::Cluster;
+use Net::Proxmox::VE::Nodes;
+use Net::Proxmox::VE::Pools;
+use Net::Proxmox::VE::Storage;
 
-%args = (
+
+
+
+my %args = (
 	host     => '10.13.37.200',
 	password => 'puk48169',
 	user     => 'root', # optional
@@ -10,9 +20,20 @@ use Net::Proxmox::VE;
 	realm    => 'pam',  # optional
 );
 
-$host = Net::Proxmox::VE->new(%args);
+my $host = Net::Proxmox::VE->new(%args);
 
-print $host->login()
-	? "Connected\n"
-	: "Could not login.\n"
+print $host -> login()
+	? "Connected.\n"
+	: "Could not login.\n";
+
+my $ok = "";
+$ok = $host -> get.nodes();
+
+print "$ok";
+
+#	? "INFO: List Nodes Successful\n"
+#	: "WARNING: List Nodes Failed\n";
+
+# $ok = $host->get_nodes('100');
+# print "$ok\n";
 
